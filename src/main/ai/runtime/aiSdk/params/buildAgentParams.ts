CherryHQ/cherry-studio

fix(cherry-cloud): enforce feature admission in provider adapter
Signed-off-by: zhibisora <73344387+zhibisora@users.noreply.github.com>

src/main/ai/runtime/aiSdk/params/buildAgentParams.ts
@@ -52,7 +52,7 @@ import { registry, ToolRegistry } from '../../../tools/adapters/aiSdk/registry'
 import { createAiRepair } from '../../../tools/adapters/aiSdk/repair'
 import type { ToolEntry } from '../../../tools/adapters/aiSdk/types'
 import { resolveConfiguredPaintingModel } from '../../../tools/painting'
-import type { AiBaseRequest, CallOverrides } from '../../../types'
+import type { AiBaseRequest, CallOverrides, ModelUsageFeature } from '../../../types'
 import {
   adjustMaxOutputTokensForReasoning,
   filterStandardParams,
@@ -103,6 +103,7 @@ export interface BuildAgentParamsInput {
   provider: Provider
   model: Model
   assistant?: Assistant
+  modelUsageFeature?: ModelUsageFeature
   /** Caller-supplied features merged after `INTERNAL_FEATURES`. */
   extraFeatures?: readonly RequestFeature[]
   /** Late-bound request usage middleware for nested tool-repair calls. */
@@ -127,7 +128,16 @@ export interface BuiltAgentParams {
 }
 
 export async function buildAgentParams(input: BuildAgentParamsInput): Promise<BuiltAgentParams> {
-  const { request, signal, provider, model, assistant, extraFeatures, compactionSink } = input
+  const {
+    request,
+    signal,
+    provider,
+    model,
+    assistant,
+    modelUsageFeature = 'chat',
+    extraFeatures,
+    compactionSink
+  } = input
 
   const resolvedEndpoint = resolveEffectiveEndpoint(provider, model)
   const { sdkConfig, credentialReceipt } = await resolveSdkConfig(
@@ -135,7 +145,8 @@ export async function buildAgentParams(input: BuildAgentParamsInput): Promise<Bu
     model,
     resolvedEndpoint,
     request.apiKeyOverride,
-    request.chatId
+    request.chatId,
+    modelUsageFeature
   )
   applyHttpTrace(sdkConfig, request.chatId, model)
   // Prefer the request-carried retained context: the persistent chat provider
@@ -347,12 +358,14 @@ async function resolveSdkConfig(
   model: Model,
   resolvedEndpoint: ResolvedEndpoint,
   apiKeyOverride?: string,
-  sessionId?: string
+  sessionId?: string,
+  modelUsageFeature?: ModelUsageFeature
 ): Promise<{ sdkConfig: SdkConfig; credentialReceipt: ServingCredentialReceipt }> {
   const { config, credentialReceipt } = await resolveProviderAiSdkConfig(provider, model, {
     apiKeyOverride,
     resolvedEndpoint,
-    sessionId
+    sessionId,
+    modelUsageFeature
   })
   return {
     sdkConfig: {

src/main/ai/runtime/aiSdk/retry/buildFallbackModels.ts
@@ -7,28 +7,27 @@
  * providerOptions / headers) — not the primary's. Fallbacks that are the active
  * model, were deleted, or can't support the request shape (native media/tools)
  * are skipped with diagnostics. Unexpected model-resolution errors still fail
- * the request; a failed Cherry Cloud availability refresh skips that fallback.
+ * the request; a provider-declared unavailable model skips only that fallback.
  * Returns `[]` when retry is disabled or unconfigured.
  *
  * Note: the primary's tools + system are kept (the agent loop is built around
  * them and ai-retry can't re-shape them mid-call); the capability gate ensures a
  * skipped fallback never receives a native request shape it can't handle.
  */
-import { application } from '@application'
 import { type AiPlugin, resolveLanguageModel } from '@cherrystudio/ai-core'
 import { loggerService } from '@logger'
 import type { ServingCredentialReceipt } from '@main/ai/provider/credential'
+import { ModelUnavailableError } from '@main/ai/provider/ModelUnavailableError'
 import { modelService } from '@main/data/services/ModelService'
 import { providerService } from '@main/data/services/ProviderService'
 import { isAbortError } from '@main/utils/error'
 import { ErrorCode, isDataApiError } from '@shared/data/api/errors'
-import { isManagedCherryCloudModel } from '@shared/data/presets/cherryai'
 import type { Assistant } from '@shared/data/types/assistant'
 import { isUniqueModelId, type Model, parseUniqueModelId, type UniqueModelId } from '@shared/data/types/model'
 import type { Provider } from '@shared/data/types/provider'
 import { isAudioModel, isFunctionCallingModel, isVideoModel, isVisionModel } from '@shared/utils/model'
 
-import type { AiBaseRequest, AppProviderSettingsMap } from '../../../types'
+import type { AiBaseRequest, AppProviderSettingsMap, ModelUsageFeature } from '../../../types'
 import type { AgentOptions } from '../loop/types'
 import { buildAgentParams } from '../params/buildAgentParams'
 import type { RequestFeature } from '../params/feature'
@@ -42,6 +41,7 @@ export interface BuildFallbackModelsArgs {
   // Base request shape accepted by `buildAgentParams`; kept `messages`-agnostic so
   // both streamText (UIMessage[]) and generateText (ModelMessage[]) requests fit.
   request: AiBaseRequest & { chatId?: string; messageId?: string }
+  modelUsageFeature: ModelUsageFeature
   assistant: Assistant | undefined
   signal: AbortSignal | undefined
   /** Primary model's stored UniqueModelId — fallbacks equal to it are dropped. */
@@ -125,26 +125,6 @@ async function resolveFallback(
   uniqueModelId: UniqueModelId,
   args: BuildFallbackModelsArgs
 ): Promise<RetryFallback | null> {
-  const { providerId } = parseUniqueModelId(uniqueModelId)
-  if (isManagedCherryCloudModel(providerId)) {
-    let availability
-    try {
-      availability = await application.get('CherryCloudService').syncEntitledModelsIfStale()
-    } catch (error) {
-      logger.warn('skipping Cherry Cloud fallback because chat availability refresh failed', error as Error, {
-        uniqueModelId
-      })
-      return null
-    }
-    if (
-      !availability.availableModelIdsByFeature.chat.includes(uniqueModelId) ||
-      availability.quotaExhaustedModelIds.includes(uniqueModelId)
-    ) {
-      logger.info('skipping unavailable Cherry Cloud fallback', { uniqueModelId })
-      return null
-    }
-  }
-
   const configured = resolveConfiguredFallback(uniqueModelId)
   if (!configured) return null
   const { provider, model } = configured
@@ -168,15 +148,24 @@ async function resolveFallback(
   }
 
   const repairUsagePlugins: { current?: AiPlugin[] } = {}
-  const { sdkConfig, credentialReceipt, plugins, options, nativeFileSupport } = await buildAgentParams({
-    request: args.request,
-    signal: args.signal,
-    provider,
-    model,
-    assistant: args.assistant,
-    extraFeatures: args.extraFeatures,
-    getRepairUsagePlugins: () => repairUsagePlugins.current ?? []
-  })
+  let built
+  try {
+    built = await buildAgentParams({
+      request: args.request,
+      signal: args.signal,
+      provider,
+      model,
+      assistant: args.assistant,
+      modelUsageFeature: args.modelUsageFeature,
+      extraFeatures: args.extraFeatures,
+      getRepairUsagePlugins: () => repairUsagePlugins.current ?? []
+    })
+  } catch (error) {
+    if (!(error instanceof ModelUnavailableError)) throw error
+    logger.warn('skipping unavailable fallback model', error, { uniqueModelId })
+    return null
+  }
+  const { sdkConfig, credentialReceipt, plugins, options, nativeFileSupport } = built
   const unsupportedNativeType = (Object.keys(required) as Array<keyof NativeFileSupport>).find(
     (type) => required[type] && !nativeFileSupport[type]
   )

Prometheus-AGS/the-boss

src/main/ai/runtime/aiSdk/params/buildAgentParams.ts
@@ -48,7 +48,7 @@ import { registry, ToolRegistry } from '../../../tools/adapters/aiSdk/registry'
 import { createAiRepair } from '../../../tools/adapters/aiSdk/repair'
 import type { ToolEntry } from '../../../tools/adapters/aiSdk/types'
 import { resolveConfiguredPaintingModel } from '../../../tools/painting'
-import type { AiChatRequest, CallOverrides } from '../../../types'
+import type { AiChatRequest, CallOverrides, ModelUsageFeature } from '../../../types'
 import {
   adjustMaxOutputTokensForReasoning,
   filterStandardParams,
@@ -98,6 +98,7 @@ export interface BuildAgentParamsInput {
   provider: Provider
   model: Model
   assistant?: Assistant
+  modelUsageFeature?: ModelUsageFeature
   /** Caller-supplied features merged after `INTERNAL_FEATURES`. */
   extraFeatures?: readonly RequestFeature[]
   /** Late-bound request usage middleware for nested tool-repair calls. */
@@ -122,14 +123,24 @@ export interface BuiltAgentParams {
 }
 
 export async function buildAgentParams(input: BuildAgentParamsInput): Promise<BuiltAgentParams> {
-  const { request, signal, provider, model, assistant, extraFeatures, compactionSink } = input
+  const {
+    request,
+    signal,
+    provider,
+    model,
+    assistant,
+    modelUsageFeature = 'chat',
+    extraFeatures,
+    compactionSink
+  } = input
 
   const resolvedEndpoint = resolveEffectiveEndpoint(provider, model)
   const { sdkConfig, credentialReceipt } = await resolveSdkConfig(
     provider,
     model,
     resolvedEndpoint,
-    request.apiKeyOverride
+    request.apiKeyOverride,
+    modelUsageFeature
   )
   applyHttpTrace(sdkConfig.providerSettings, {
     topicId: request.conversation.topicId,

src/main/ai/runtime/aiSdk/retry/buildFallbackModels.ts
@@ -7,28 +7,27 @@
  * providerOptions / headers) — not the primary's. Fallbacks that are the active
  * model, were deleted, or can't support the request shape (native media/tools)
  * are skipped with diagnostics. Unexpected model-resolution errors still fail
- * the request; a failed Cherry Cloud availability refresh skips that fallback.
+ * the request; a provider-declared unavailable model skips only that fallback.
  * Returns `[]` when retry is disabled or unconfigured.
  *
  * Note: the primary's tools + system are kept (the agent loop is built around
  * them and ai-retry can't re-shape them mid-call); the capability gate ensures a
  * skipped fallback never receives a native request shape it can't handle.
  */
-import { application } from '@application'
 import { type AiPlugin, resolveLanguageModel } from '@cherrystudio/ai-core'
 import { loggerService } from '@logger'
 import type { ServingCredentialReceipt } from '@main/ai/provider/credential'
+import { ModelUnavailableError } from '@main/ai/provider/ModelUnavailableError'
 import { modelService } from '@main/data/services/ModelService'
 import { providerService } from '@main/data/services/ProviderService'
 import { isAbortError } from '@main/utils/error'
 import { ErrorCode, isDataApiError } from '@shared/data/api/errors'
-import { isManagedCherryCloudModel } from '@shared/data/presets/cherryai'
 import type { Assistant } from '@shared/data/types/assistant'
 import { isUniqueModelId, type Model, parseUniqueModelId, type UniqueModelId } from '@shared/data/types/model'
 import type { Provider } from '@shared/data/types/provider'
 import { isAudioModel, isFunctionCallingModel, isVideoModel, isVisionModel } from '@shared/utils/model'
 
-import type { AiChatRequest, AppProviderSettingsMap } from '../../../types'
+import type { AiChatRequest, AppProviderSettingsMap, ModelUsageFeature } from '../../../types'
 import type { AgentOptions } from '../loop/types'
 import { buildAgentParams } from '../params/buildAgentParams'
 import type { RequestFeature } from '../params/feature'
@@ -42,6 +41,7 @@ export interface BuildFallbackModelsArgs {
   // Base request shape accepted by `buildAgentParams`; kept `messages`-agnostic so
   // both streamText (UIMessage[]) and generateText (ModelMessage[]) requests fit.
   request: AiChatRequest & { messageId?: string }
+  modelUsageFeature: ModelUsageFeature
   assistant: Assistant | undefined
   signal: AbortSignal | undefined
   /** Primary model's stored UniqueModelId — fallbacks equal to it are dropped. */
@@ -125,26 +125,6 @@ async function resolveFallback(
   uniqueModelId: UniqueModelId,
   args: BuildFallbackModelsArgs
 ): Promise<RetryFallback | null> {
-  const { providerId } = parseUniqueModelId(uniqueModelId)
-  if (isManagedCherryCloudModel(providerId)) {
-    let availability
-    try {
-      availability = await application.get('CherryCloudService').syncEntitledModelsIfStale()
-    } catch (error) {
-      logger.warn('skipping Cherry Cloud fallback because chat availability refresh failed', error as Error, {
-        uniqueModelId
-      })
-      return null
-    }
-    if (
-      !availability.availableModelIdsByFeature.chat.includes(uniqueModelId) ||
-      availability.quotaExhaustedModelIds.includes(uniqueModelId)
-    ) {
-      logger.info('skipping unavailable Cherry Cloud fallback', { uniqueModelId })
-      return null
-    }
-  }
-
   const configured = resolveConfiguredFallback(uniqueModelId)
   if (!configured) return null
   const { provider, model } = configured
@@ -175,15 +155,24 @@ async function resolveFallback(
   }
 
   const repairUsagePlugins: { current?: AiPlugin[] } = {}
-  const { sdkConfig, credentialReceipt, plugins, options, nativeFileSupport } = await buildAgentParams({
-    request: args.request,
-    signal: args.signal,
-    provider,
-    model,
-    assistant: args.assistant,
-    extraFeatures: args.extraFeatures,
-    getRepairUsagePlugins: () => repairUsagePlugins.current ?? []
-  })
+  let built
+  try {
+    built = await buildAgentParams({
+      request: args.request,
+      signal: args.signal,
+      provider,
+      model,
+      assistant: args.assistant,
+      modelUsageFeature: args.modelUsageFeature,
+      extraFeatures: args.extraFeatures,
+      getRepairUsagePlugins: () => repairUsagePlugins.current ?? []
+    })
+  } catch (error) {
+    if (!(error instanceof ModelUnavailableError)) throw error
+    logger.warn('skipping unavailable fallback model', error, { uniqueModelId })
+    return null
+  }
+  const { sdkConfig, credentialReceipt, plugins, options, nativeFileSupport } = built
   const unsupportedNativeType = (Object.keys(required) as Array<keyof NativeFileSupport>).find(
     (type) => required[type] && !nativeFileSupport[type]
   )